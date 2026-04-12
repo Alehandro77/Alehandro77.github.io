abd4233680b22358ddb9a7bb9463794cb3066258
Alehandro77 <shurapchta@gmail.com>
2026-04-12 12:20:09 +0300

    Modify forward and backward animations with opacity
    Updated animations for forward and backward functions, including opacity changes for the main element.

--- burger.js
+++ burger.js
@@ -5,7 +5,7 @@ let end = 0;
 function forward() {
     anime({
         targets: '.menu-small',
-        translateX: ['-100%', '0'],
+        translateX: ['-110%', '-5%'],
         easing: 'easeInOutQuad',
         direction: 'alternate',
         duration: 1000,
@@ -29,13 +29,22 @@ function forward() {
         duration: 1000,
         loop: false
     });
+
+    anime({
+        targets: 'main',
+        opacity: 0.1,       
+        duration: 1000,
+        loop: false,
+        easing: 'easeOutQuad'
+    });
+
     condition = false; 
 }
 
 function backward() {
     anime({
         targets: '.menu-small',
-        translateX: ['0', '-100%'],
+        translateX: ['-5%', '-110%'],
         easing: 'easeInOutQuad',
         direction: 'alternate',
         duration: 1000,
@@ -59,16 +68,41 @@ function backward() {
         duration: 1000,
         loop: false
     });
+
+    anime({
+        targets: 'main',
+        opacity: 1,       
+        duration: 1000,
+        loop: false,
+        easing: 'easeOutQuad'
+    });
+
     condition = true; 
 }
 
-$('.menu_small_icon').click(function () {
+
+
+
+$('.menu_small_icon').on('touchend', function () {
     if (condition) {
         forward();
     } else {
         backward();
     }
 });
+
+$('.menu_small_item').on('touchend', function () {
+    backward();
+});
+
+// $('.menu_small_icon').on('click', function () {
+//     if (condition) {
+//         forward();
+//     } else {
+//         backward();
+//     }
+// });
+
 
 $('.container').on('touchstart', function (event) {
     start = event.originalEvent.touches[0].pageX;
